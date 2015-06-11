1c0de8625c1f503cebd3a5d13dbee81a46b8a457
raingxm <raingxm@163.com>
2015-06-11 21:51:32 +0800

    add comment component

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,22 @@
+var Comment = React.createClass({
+    render: function() {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
 var CommentList = React.createClass({
     render: function() {
         return (
             <div className="commentList">
-                Hello, world! I am a CommentList.
+                <Comment author="raingxm">Happyniess journey</Comment>
+                <Comment author="David">Seven ways to have a better life</Comment>
             </div>
         );
     }
